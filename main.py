f82a263c39280be7eb3b679416065cc048043614
MarcoNapoleone <58686646+MarcoNapoleone@users.noreply.github.com>
2022-05-10 12:55:43 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,32 +1,42 @@
-from itertools import count
-
-import datetime
-import random
-import requests
-import uuid
-import socket
+import os
+import dht
+import urequests
+import ntptime
 import json
-import Adafruit_DHT
+from time import *
+import random
 
-from config import print_env
+sensor = dht.DHT22(Pin(4))
+ntptime.host = "1.europe.pool.ntp.org"
 
-DHT_SENSOR = Adafruit_DHT.DHT22
-DHT_PIN = 4
-SERVICE_URL = 'https://incomunicando.it/webservice/getdata.php'
-LAST_READING = datetime.datetime.now() - datetime.timedelta(0, 300)  # now - 5 min
-NEXT_READING_DELTA = 5  # seconds
-
-
-def get_mac():
-    return hex(uuid.getnode())
+DATA_URL = 'https://argos-iot.com/webservice/getdata.php'
+CHECK_URL = 'https://incomunicando.it/webservice/checkdata.php'
+LAST_READING = mktime((2000, 1, 1, 0, 0, 0, 0, 0))
+NEXT_READING_DELTA = 5
 
 
-def get_local_ip():
-    return socket.gethostbyname(socket.gethostname() + ".local")
+def sync_rtc():
+    try:
+        ntptime.settime()
+    except:
+        print("Error syncing time")
+
+
+def add_time_delta(date, delta_hours=0, delta_minutes=0, delta_seconds=0, delta_mseconds=0):
+    return mktime((
+        date[0],
+        date[1],
+        date[2],
+        date[3] + delta_hours,
+        date[4] + delta_minutes,
+        date[5] + delta_seconds,
+        date[6] + delta_mseconds,
+        0
+    ))
 
 
 def get_sensor_data():
-    humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+    sensor.measure()
 
     """
     :tipo_sensore:
@@ -38,10 +48,9 @@ def get_sensor_data():
         {"sensore": tipo_sensore, valore: string, um: string}
     """
 
-
     return [
-        {"sensore": "temp", "valore": temperature, "um": "C"},
-        {"sensore": "hum", "valore": humidity, "um": "%"},
+        {"sensore": "temp", "valore": sensor.temperature(), "um": "C"},
+        {"sensore": "hum", "valore": sensor.humidity(), "um": "%"},
         {"sensore": "bpt", "valore": random.randint(9500, 10300) / 10, "um": "hPa"},
         {"sensore": "aqi", "valore": random.randint(10, 6000) / 10, "um": "AQI"}
     ]
@@ -51,48 +60,46 @@ def get_payload():
     return {
         "id": "25555",
         "dispositivo": {
-            "mac": get_mac(),
-            "ip": get_local_ip()
+            "mac": '0000.0000.0000.0000',
+            "ip": lan.ifconfig()[0]
         },
         "datiRilevazione": get_sensor_data(),
-        "tempoRilevazione": datetime.datetime.now().strftime("%Y-%m-%d (%H:%M:%S.%f)")
+        "tempoRilevazione": mktime(localtime()),
     }
 
 
-def send_data(data, times):
-    if times > 0:
-        try:
-            response = requests.put(SERVICE_URL, json=data)
-            return response
-        except requests.exceptions.Timeout:
-            send_data(data, times - 1)
-        except requests.exceptions.TooManyRedirects:
-            print('error 404')
-        except requests.exceptions.RequestException as e:
-            print(e)
+def core_loop():
+    global LAST_READING
+    global NEXT_READING_DELTA
+
+    tries = 3
+    success = False
+
+    if mktime(localtime()) > add_time_delta(localtime(LAST_READING), delta_seconds=NEXT_READING_DELTA):
+        while not success and tries > 0:
+            try:
+                r = urequests.post(DATA_URL, json=get_payload())
+                if r.status_code in [200, 201]:
+                    print(r.json())
+                    data = json.loads(r.text)
+                    LAST_READING = mktime(localtime())
+                    NEXT_READING_DELTA = int(data['data']['prossimaRilvazione'])
+                    success = True
+                else:
+                    print('HTTP request failed: {} {}'.format(r.status_code, r.reason))
+            except OSError as e:
+                print('network error: {}'.format(e.errno))
+                tries -= 1
+                pass
+    else:
+        print('Awaiting...')
+        sleep(int(NEXT_READING_DELTA / 3))
 
 
 def main():
-    global LAST_READING
-    global NEXT_READING_DELTA
-    error = 0
-
-    while not error:
-        # if time from last reading is bigger than delta
-        if datetime.datetime.now() > LAST_READING + datetime.timedelta(0, NEXT_READING_DELTA):
-            response = send_data(get_payload(), 2).text  # todo handle errors
-            if response is not None:
-                print(response)
-                data = json.loads(response)
-                LAST_READING = datetime.datetime.now()
-                if data['status']:
-                    NEXT_READING_DELTA = int(data['data']['prossimaRilvazione'])
-                else:
-                    print('status error')
-            else:
-                print('error no response')
+    sync_rtc()
+    while 1:
+        core_loop()
 
 
-if __name__ == "__main__":
-    print_env()
-    main()
+main()
